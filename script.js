843b3c9be903e86b2fe0604350fe848ebd85db1e
Adarsh11234 <aadarshpatil123@gmail.com>
2024-11-27 20:18:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  
+	  str = str.trim();
+
+    
+    if (str === "") {
+        return "";
+    }
+
+    
+    let spaceIndex = str.indexOf(" ");
+
+  
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    
+    return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
+// Examples
+console.log(firstWord('see and stop')); 
+console.log(firstWord(' Hello World!')); 
+console.log(firstWord('12345')); 
+console.log(firstWord('')); 
+}
+
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
